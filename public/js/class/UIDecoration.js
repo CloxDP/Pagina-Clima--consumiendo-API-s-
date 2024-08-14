e826e8763b469630e75ebaca426d6a5560e84596
Angel Juaben Inyena Pasuy Muchavisoy <apasuy@unal.edu.co>
2024-08-14 00:04:49 -0500

    aall backgrounds

--- public/js/class/UIDecoration.js
+++ public/js/class/UIDecoration.js
@@ -44,26 +44,26 @@ export class UIDecoration{
     setVideo(){
         document.body.style.background = '';
         if (this.id >= 200 && this.id < 300) {
-            return 'https://openweathermap.org/img/wn/11d@2x.png'
+            document.body.style.backgroundImage = 'url(img/weatherGifs/Thunderstorm.gif)';
         } else if ((this.id >= 300 && this.id < 400) ||(this.id >= 520 && this.id < 600)) {
-            return 'https://openweathermap.org/img/wn/09d@2x.png'
+            document.body.style.backgroundImage = 'url(img/weatherGifs/drizzle.gif)';
         } else if (this.id >= 500 && this.id <=504) {
-
+            document.body.style.backgroundImage = 'url(img/weatherGifs/Rain.gif)';
         } else if ((this.id >= 600 && this.id < 700)|| this.id===511) {
             document.body.style.backgroundImage = 'url(img/weatherGifs/snow.gif)';
         } else if (this.id >= 700 && this.id < 800) {
-            
+            document.body.style.backgroundImage = 'url(img/weatherGifs/mist.gif)';
         } else if (this.id===800) {
             if(this.hour>=6 && this.hour<=18){
-                return 'https://openweathermap.org/img/wn/01d@2x.png'
+                document.body.style.backgroundImage = 'url(img/weatherGifs/day.gif)';
             }else{
-                return 'https://openweathermap.org/img/wn/01n@2x.png'
+                document.body.style.backgroundImage = 'url(img/weatherGifs/night.gif)';
             }
         } else if (this.id===801) {
             if(this.hour>=6 && this.hour<=18){
-                return 'https://openweathermap.org/img/wn/02d@2x.png'
+                document.body.style.backgroundImage = 'url(img/weatherGifs/clouds-d.gif)';
             }else{
-                return 'https://openweathermap.org/img/wn/02n@2x.png'
+                document.body.style.backgroundImage = 'url(img/weatherGifs/clouds-n.gif)';
             }
         }else if (this.id===802) {
             document.body.style.backgroundImage = 'url(img/weatherGifs/scatteredClouds.gif)';
